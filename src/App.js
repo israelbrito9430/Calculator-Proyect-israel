db8eb0309897a76ca5ef000f3ac00982cfba88f8
israelbrito9430 <israelbrito9430@gmail.com>
2022-01-25 21:48:58 -0500

    calculator sum

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useState } from 'react';
+import './App.scss';
 
 function App() {
+  const [number1,setNumber1] = useState(0);
+  const [number2,setNumber2] = useState(0);
+  const [operation,setOperation] = useState('sumar');
+  const [result,setResult] = useState(0);
+  const calculate = operation => {
+    let resultCalculated = 0;
+    if(operation === 'sumar'){
+      resultCalculated = Number(number1) + Number(number2)
+    }
+    setResult(resultCalculated)
+    setNumber1(0)
+    setNumber2(0)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <select value={operation} onChange={(value) => setOperation(value)}>
+        <option value='sumar'>Sumar</option>
+        <option>Restar</option>
+        <option>Multiplicar</option>
+        <option>Dividir</option>
+      </select>
+      <label>
+        Number 1
+        <input type='number' value={number1} onChange={(e) => setNumber1(e.target.value)}></input>
+      </label>
+      <label>
+        Number 2
+        <input type='number' value={number2} onChange={(e) => setNumber2(e.target.value)}></input>
+      </label>
+      <button onClick={() => calculate(operation)}>Calcular</button>
+      <h1>{result}</h1>
     </div>
   );
 }
